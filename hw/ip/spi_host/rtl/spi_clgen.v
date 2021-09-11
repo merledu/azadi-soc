f997362acc91b022557d3a5be69fbe9c667fb647
sajjadahmed677 <merledu677@gmail.com>
2021-09-11 19:38:45 +0500

    extra spaces and indentations removed

--- hw/ip/spi_host/rtl/spi_clgen.v
+++ hw/ip/spi_host/rtl/spi_clgen.v
@@ -23,25 +23,25 @@ module spi_clgen (
 	assign cnt_zero = cnt == {16 {1'b0}};
 	assign cnt_one = cnt == {{15 {1'b0}}, 1'b1};
 	always @(posedge clk_i or negedge rst_ni)
-		if (~rst_ni)
-			cnt <= {16 {1'b1}};
-		else if (!enable || cnt_zero)
-			cnt <= divider;
-		else
-			cnt <= cnt - {{15 {1'b0}}, 1'b1};
+	if (~rst_ni)
+		cnt <= {16 {1'b1}};
+	else if (!enable || cnt_zero)
+		cnt <= divider;
+	else
+		cnt <= cnt - {{15 {1'b0}}, 1'b1};
 	always @(posedge clk_i or negedge rst_ni)
-		if (~rst_ni)
-			clk_out <= 1'b0;
-		else
-			clk_out <= ((enable && cnt_zero) && (!last_clk || clk_out) ? ~clk_out : clk_out);
+	if (~rst_ni)
+		clk_out <= 1'b0;
+	else
+		clk_out <= ((enable && cnt_zero) && (!last_clk || clk_out) ? ~clk_out : clk_out);
 	always @(posedge clk_i or negedge rst_ni)
-		if (~rst_ni) begin
-			pos_edge <= 1'b0;
-			neg_edge <= 1'b0;
-		end
-		else begin
-			pos_edge <= (((enable && !clk_out) && cnt_one) || (!(|divider) && clk_out)) || ((!(|divider) && go) && !enable);
-			neg_edge <= ((enable && clk_out) && cnt_one) || ((!(|divider) && !clk_out) && enable);
-		end
+	if (~rst_ni) begin
+		pos_edge <= 1'b0;
+		neg_edge <= 1'b0;
+	end
+	else begin
+		pos_edge <= (((enable && !clk_out) && cnt_one) || (!(|divider) && clk_out)) || ((!(|divider) && go) && !enable);
+		neg_edge <= ((enable && clk_out) && cnt_one) || ((!(|divider) && !clk_out) && enable);
+	end
 endmodule
  
