f9ce93432371de93104edc776758476c3f293df2
Auringzaib Sabir <auringzaibsabir11@gmail.com>
2022-01-28 16:24:24 +0500

    Implemented ignore_bins for sign extended RV32B and bitcounting RV32B

--- hw/vendor/lowrisc_ibex/rtl/ibex_decoder.sv
+++ hw/vendor/lowrisc_ibex/rtl/ibex_decoder.sv
@@ -1798,7 +1798,7 @@ module ibex_decoder #(
     ALU_OPERATIONS: coverpoint alu_operator_o{
       ignore_bins ignore_vals = {ALU_XNOR, ALU_ORN, ALU_ANDN, ALU_SRO, ALU_SLO, ALU_ROR, ALU_ROL, ALU_GREV, 
                                  ALU_GORC, ALU_SHFL, ALU_UNSHFL, ALU_MIN, ALU_MINU, ALU_MAX, ALU_MAXU, ALU_PACK,
-                                 ALU_PACKU, ALU_PACKH};
+                                 ALU_PACKU, ALU_PACKH, ALU_SEXTB, ALU_SEXTH, ALU_CLZ, ALU_CTZ, ALU_PCNT};
     }
   endgroup : alu_cg
 
