dff73afdede783ff34b538b2bc51930847c8bcd8
Zeeshan Rafique <zeeshanrafique23@gmail.com>
2022-03-13 00:10:26 +0500

    Updated sky130 mem
    Signed-off-by: Zeeshan Rafique <zeeshanrafique23@gmail.com>

--- hw/vendor/openram/rtl/sky130_sram_1kbyte_1rw1r_32x256_8.v
+++ hw/vendor/openram/rtl/sky130_sram_1kbyte_1rw1r_32x256_8.v
@@ -54,7 +54,8 @@ module sky130_sram_1kbyte_1rw1r_32x256_8(
     wmask0_reg = wmask0;
     addr0_reg = addr0;
     din0_reg = din0;
-    #(T_HOLD) dout0 = 32'bx;
+    // #(T_HOLD) 
+    dout0 = 32'bx;
     if ( !csb0_reg && web0_reg && VERBOSE ) 
       $display($time," Reading %m addr0=%b dout0=%b",addr0_reg,mem[addr0_reg]);
     if ( !csb0_reg && !web0_reg && VERBOSE )
@@ -72,7 +73,8 @@ module sky130_sram_1kbyte_1rw1r_32x256_8(
     addr1_reg = addr1;
     if (!csb0 && !web0 && !csb1 && (addr0 == addr1))
          $display($time," WARNING: Writing and reading addr0=%b and addr1=%b simultaneously!",addr0,addr1);
-    #(T_HOLD) dout1 = 32'bx;
+    // #(T_HOLD);
+    dout1 = 32'bx;
     if ( !csb1_reg && VERBOSE ) 
       $display($time," Reading %m addr1=%b dout1=%b",addr1_reg,mem[addr1_reg]);
   end
