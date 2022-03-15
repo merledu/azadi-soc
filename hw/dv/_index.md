50fe7e1eb03d2c63b8b126bf2197509d99b7c967
Zeeshan Rafique <zeeshanrafique23@gmail.com>
2022-03-15 10:53:44 +0500

    remove unused files
    Signed-off-by: Zeeshan Rafique <zeeshanrafique23@gmail.com>

--- hw/dv/_index.md
+++
@@ -1,5 +0,0 @@
----
-title: "Design Verification"
----
-
-{{% sectionContent %}}
--- hw/dv/sv/csrng_agent/seq_lib/csrng_seq_list.sv
+++
@@ -1,7 +0,0 @@
-// Copyright lowRISC contributors.
-// Licensed under the Apache License, Version 2.0, see LICENSE for details.
-// SPDX-License-Identifier: Apache-2.0
-
-`include "csrng_base_seq.sv"
-`include "csrng_host_seq.sv"
-`include "csrng_device_seq.sv"
