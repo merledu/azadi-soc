3293ebfd5d76ef0158a6de812b566dd00c0ed18f
Zeeshan Rafique <zeeshanrafique23@gmail.com>
2022-04-06 14:20:19 +0500

    Fix warning
    Signed-off-by: Zeeshan Rafique <zeeshanrafique23@gmail.com>

--- hw/vendor/openram/rtl/sky130_sram_1kbyte_1rw1r_32x256_8.v
+++ hw/vendor/openram/rtl/sky130_sram_1kbyte_1rw1r_32x256_8.v
@@ -75,7 +75,7 @@ module sky130_sram_1kbyte_1rw1r_32x256_8(
          $display($time," WARNING: Writing and reading addr0=%b and addr1=%b simultaneously!",addr0,addr1);
     // #(T_HOLD);
     dout1 = 32'bx;
-    if ( !csb1_reg && VERBOSE ) 
+    // if ( !csb1_reg && VERBOSE ) 
       // $display($time," Reading %m addr1=%b dout1=%b",addr1_reg,mem[addr1_reg]);
   end
 
