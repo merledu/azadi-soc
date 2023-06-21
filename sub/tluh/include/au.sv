6e378496f6a19f8b006e4f6af505fc4f9ee665bb
Doaa Magdy <71094316+doaamagdy2024@users.noreply.github.com>
2023-06-21 03:57:15 +0300

    modifying the au module

--- sub/tluh/include/au.sv
+++ sub/tluh/include/au.sv
@@ -12,11 +12,28 @@ module arithmetic_unit
     );
 
     //. 1: min, 2: max, 3: minu, 4: maxu, 5: add
-    assign result = (op == tluh_pkg::MIN) ? $signedmin(a, b) :
-                    (op == tluh_pkg::MAX) ? $signedmax(a, b) :
-                    (op == tluh_pkg::MINU) ? $unsignedmin(a, b) :
-                    (op == tluh_pkg::MAXU) ? $unsignedmax(a, b) :
-                    (op == tluh_pkg::ADD) ? (a + b) : 0;
+
+    always_comb begin : blockName
+        case (op)
+            tluh_pkg::MIN: result = $signedmin(a, b);
+            tluh_pkg::MAX: result = $signedmax(a, b);
+            tluh_pkg::MINU: result = $unsignedmin(a, b);
+            tluh_pkg::MAXU: result = $unsignedmax(a, b);
+            tluh_pkg::ADD: begin 
+                {cout, result} = a + b + cin;
+            end
+            default: result = 0; //. to avoid latches
+        endcase
+        
+    end
+
+    // assign result = (op == tluh_pkg::MIN) ? $signedmin(a, b) :
+    //                 (op == tluh_pkg::MAX) ? $signedmax(a, b) :
+    //                 (op == tluh_pkg::MINU) ? $unsignedmin(a, b) :
+    //                 (op == tluh_pkg::MAXU) ? $unsignedmax(a, b) :
+    //                 (op == tluh_pkg::ADD) ? (a + b) : 0;
+
+
     //. TO ASK: what about the carry in case of add? Should we add a carry in port?
     //. TO ASK: is it ok to simply use the $unsignedmin/max functions?
     //. and also what about the + > < operators? are they ok? or we have to implement another adder (carry save adder, carry bypass adder, etc.) ?
