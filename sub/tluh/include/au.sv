1d4043366070f014e52aa61369aca9caee0f37a9
Doaa Magdy <71094316+doaamagdy2024@users.noreply.github.com>
2023-06-21 03:38:51 +0300

    add cin and cout as ports to the arith. unit module to allow making operations in 2 steps

--- sub/tluh/include/au.sv
+++ sub/tluh/include/au.sv
@@ -1,12 +1,17 @@
 module arithmetic_unit
     (
+        //. TO ASK: what if the operation is done on 64 bits? should we perform the operatoin in 2 steps?
+        //. So, we have to out the carry out of the first operation and use it as carry in for the second operation?
+        //. Or we can simply use the 64 bits operators?
         input logic [31:0] a,
         input logic [31:0] b,
+        input bit cin, //. carry in
         input tluh_pkg::tluh_a_param_arith op,
-        output logic [31:0] result
+        output logic [31:0] result,
+        output bit cout //. carry out
     );
 
-    // 1: min, 2: max, 3: minu, 4: maxu, 5: add
+    //. 1: min, 2: max, 3: minu, 4: maxu, 5: add
     assign result = (op == tluh_pkg::MIN) ? $signedmin(a, b) :
                     (op == tluh_pkg::MAX) ? $signedmax(a, b) :
                     (op == tluh_pkg::MINU) ? $unsignedmin(a, b) :
