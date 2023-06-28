49488bcb9db151abff653819998a59222e92dbd4
Doaa Magdy <71094316+doaamagdy2024@users.noreply.github.com>
2023-06-28 03:58:20 +0300

    modifying the au module: add enable signal & modify the width of the operands to be in terms of tluh_pkg parameters

--- sub/tluh/include/au.sv
+++ sub/tluh/include/au.sv
@@ -3,27 +3,30 @@ module arithmetic_unit
         //. TO ASK: what if the operation is done on 64 bits? should we perform the operatoin in 2 steps?
         //. So, we have to out the carry out of the first operation and use it as carry in for the second operation?
         //. Or we can simply use the 64 bits operators?
-        input logic [31:0] a,
-        input logic [31:0] b,
-        input bit cin, //. carry in
-        input tluh_pkg::tluh_a_param_arith op,
-        output logic [31:0] result,
-        output bit cout //. carry out
+        input bit                          enable_i,
+        input logic [tluh_pkg::TL_DW-1:0]  op1_i,
+        input logic [tluh_pkg::TL_DW-1:0]  op2_i,
+        input bit                          cin_i, //. carry in
+        input tluh_pkg::tluh_a_param_arith operation_i,
+        output logic [tluh_pkg::TL_DW-1:0] result_o,
+        output bit                         cout_o //. carry out
     );
 
     //. 1: min, 2: max, 3: minu, 4: maxu, 5: add
 
-    always_comb begin : blockName
-        case (op)
-            tluh_pkg::MIN: result = $signedmin(a, b);
-            tluh_pkg::MAX: result = $signedmax(a, b);
-            tluh_pkg::MINU: result = $unsignedmin(a, b);
-            tluh_pkg::MAXU: result = $unsignedmax(a, b);
-            tluh_pkg::ADD: begin 
-                {cout, result} = a + b + cin;
-            end
-            default: result = 0; //. to avoid latches
-        endcase
+    always_comb begin : perform_operation
+        if(enable_i) begin
+            case (operation_i)
+                tluh_pkg::MIN: result_o  = $signedmin(op1_i, op2_i);
+                tluh_pkg::MAX: result_o  = $signedmax(op1_i, op2_i);
+                tluh_pkg::MINU: result_o = $unsignedmin(op1_i, op2_i);
+                tluh_pkg::MAXU: result_o = $unsignedmax(op1_i, op2_i);
+                tluh_pkg::ADD: begin 
+                    {cout_o, result_o} = op1_i + op2_i + cin_i;
+                end
+                default: result_o = 0; //. to avoid latches
+            endcase
+        end
         
     end
 
